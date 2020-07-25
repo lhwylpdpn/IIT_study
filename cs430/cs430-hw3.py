0d1c33f8e6cdef780552dc284d0fffa8226f43cf
liuhao <58254451@qq.com>
2020-07-25 17:32:03 +0800

    cs430-hw3-DP

--- cs430/cs430-hw3.py
+++ cs430/cs430-hw3.py
@@ -33,11 +33,11 @@ class Solution1(object):
 class Solution(object):
  
 
-    def __init__(self,grid):
+    def __init__(self,cell):
 
         
         n=[[-1]*(len(grid[0])) for i in range(len(grid))]
-        print(self.recursively_withoutmem(grid,i=0,j=0))
+        print(self.recursively_withoutmem(cell,i=0,j=0))
         
     def maxValue(self,grid):
         
@@ -53,19 +53,19 @@ class Solution(object):
             return max(grid[0][0]+self.maxValue(grid[1:len(grid)]),grid[0][0]+self.maxValue([grid[x][1:len(grid[x])] for  x in range(0,len(grid))]))
 
 
-    def recursively_withoutmem(self,grid,i,j):
+    def recursively_withoutmem(self,cell,i,j):
         
       
-            if i==len(grid)-1 and j==len(grid[0])-1:
+            if i==len(cell)-1 and j==len(cell[0])-1:
 
-                return grid[len(grid)-1][len(grid[0])-1]
-            if i>=len(grid)-1: #move right
+                return cell[len(cell)-1][len(cell[0])-1]
+            if i>=len(cell)-1: #move right
                
-                return grid[i][j]+self.recursively_withoutmem(grid,i,j+1)
-            if j>=len(grid[0])-1:# move down
-                return grid[i][j]+self.recursively_withoutmem(grid,i+1,j)
+                return cell[i][j]+self.recursively_withoutmem(cell,i,j+1)
+            if j>=len(cell[0])-1:# move down
+                return cell[i][j]+self.recursively_withoutmem(cell,i+1,j)
             
-            return max(grid[i][j]+self.recursively_withoutmem(grid,i,j+1),grid[i][j]+self.recursively_withoutmem(grid,i+1,j))
+            return max(cell[i][j]+self.recursively_withoutmem(cell,i,j+1),cell[i][j]+self.recursively_withoutmem(cell,i+1,j))
            
 
     def maxValue_4(self,grid,i,j,n):
@@ -99,28 +99,62 @@ class Solution(object):
             #return max(grid[i][j]+self.maxValue_3(grid,i,j+1),grid[i][j]+self.maxValue_3(grid,i+1,j))
 
 
-    def iteratively_withmem(self,grid):
+    def iteratively_withmem(self,cell):
         
       
 
-            for i in range(0,len(grid)):
+            for i in range(0,len(cell)):
 
-                for j in range(0,len(grid[0])):
+                for j in range(0,len(cell[0])):
 
                     if i==0 and j==0:
                         continue
                     if i==0:
-                        grid[i][j]+=grid[i][j-1]
+                        cell[i][j]+=cell[i][j-1]
                     elif j==0:
-                        grid[i][j]+=grid[i-1][j]
+                        cell[i][j]+=cell[i-1][j]
                     else:
-                        grid[i][j]=max(grid[i][j-1]+grid[i][j],grid[i-1][j]+grid[i][j])
+                        cell[i][j]=max(cell[i][j-1]+cell[i][j],cell[i-1][j]+cell[i][j])
 
-            return grid[len(grid)-1][len(grid[0])-1]
-
-
+            return cell[len(cell)-1][len(cell[0])-1]
 
 
+
+def recur_nomem(cell,i,j):
+        
+      
+            if i==len(cell)-1 and j==len(cell[0])-1:
+                return cell[len(cell)-1][len(cell[0])-1]
+            if i>=len(cell)-1: # only can move right
+                return cell[i][j]+recur_nomem(cell,i,j+1)
+            if j>=len(cell[0])-1:# only can move down
+                return cell[i][j]+recur_nomem(cell,i+1,j)
+            
+            return max(cell[i][j]+recur_nomem(cell,i,j+1),cell[i][j]+recur_nomem(cell,i+1,j))
+           
+def main(cell):
+
+    return(recur_nomem(cell,i=0,j=0))
+
+
+def iteratively_withmem(cell):
+        
+      
+
+            for i in range(0,len(cell)):
+
+                for j in range(0,len(cell[0])):
+
+                    if i==0 and j==0:
+                        continue
+                    if i==0:
+                        cell[i][j]+=cell[i][j-1]
+                    elif j==0:
+                        cell[i][j]+=cell[i-1][j]
+                    else:
+                        cell[i][j]=max(cell[i][j-1]+cell[i][j],cell[i-1][j]+cell[i][j])
+
+            return cell[len(cell)-1][len(cell[0])-1]
 
 if __name__ == '__main__':
     grid=[[7,1,3,5,8,9,9,2,1,9,0,8,3,1,6,6,9,5],[9,5,9,4,0,4,8,8,9,5,7,3,6,6,6,9,1,6],[8,2,9,1,3,1,9,7,2,5,3,1,2,4,8,2,8,8],[6,7,9,8,4,8,3,0,4,0,9,6,6,0,0,5,1,4],[7,1,3,1,8,8,3,1,2,1,5,0,2,1,9,1,1,4],[9,5,4,3,5,6,1,3,6,4,9,7,0,8,0,3,9,9],[1,4,2,5,8,7,7,0,0,7,1,2,1,2,7,7,7,4],[3,9,7,9,5,8,9,5,6,9,8,8,0,1,4,2,8,2],[1,5,2,2,2,5,6,3,9,3,1,7,9,6,8,6,8,3],[5,7,8,3,8,8,3,9,9,8,1,9,2,5,4,7,7,7],[2,3,2,4,8,5,1,7,2,9,5,2,4,2,9,2,8,7],[0,1,6,1,1,0,0,6,5,4,3,4,3,7,9,6,1,9]]
@@ -130,9 +164,4 @@ if __name__ == '__main__':
 #   [1,5,1],
 #   [4,2,1]
 # ]
-    a=Solution(grid)
-    p=a.iteratively_withmem(grid)
-    print(p)
-    #b=Solution1()
-    #print(b.maxValue(grid))
-    #print(a.maxValue_3(grid))
+    print(iteratively_withmem(grid))
