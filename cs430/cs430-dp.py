0a8f9d8c7d73a78bc981a389020f425cb7a39681
liuhao <58254451@qq.com>
2020-08-04 21:32:38 +0800

    33

--- cs430/cs430-dp.py
+++ cs430/cs430-dp.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import time
-class Solution(object):
+class Solution():#q6
+
     def __init__(self,n):
         self.dp=[0]*(n+1)
         
@@ -30,16 +31,60 @@ class Solution(object):
                 dp[m]+=dp[i-1]*dp[m-i]
         return dp[n]
 
+class Q7:
+
+    def delete_element(self,nums):
+
+        i=0#kuai
+        j=0#man
+
+        for i in range(1,len(nums)):
+            
+            if nums[i]!=nums[j]:
+                j+=1
+                nums[j]=nums[i]
+        print(nums[0:j+1])
+        return j+1
+                
+class Q8:
+
+    def return_sum_limit(self,nums,s):
+
+        start=0
+        end=0
+        sum_=0
+        min_=len(nums)+1
+        for x in range(0,len(nums)):
+            sum_+=nums[x]
+            
+            
+            while sum_>=s:
+                if end-start+1<=min_:
+                    min_=end-start+1
+                sum_=sum_-nums[start]
+                start+=1
+            end+=1
+        return 0 if min_==len(nums)+1 else min_
+
+
+
+
+ 
+
 
 if __name__ == '__main__':
-    n=30
-    a=time.time()
-    res=Solution(n)
-    t=res.numTrees(n)
+
+    n=[2,3,1,2,4,3]
+
+    #res=Q7()
+    #n=[1,2,2,3]
+    #print(res.delete_element(n))
+    res=Q8()
+    print(res.return_sum_limit(n,7))
+
     
-    print(t)
-    print(time.time()-a)
-    b=time.time()
-    x=res.numTrees_no_recursion(n)
-    print(x)
-    print(time.time()-b)
+
+
+
+
+    
