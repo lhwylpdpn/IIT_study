0f54b5ad2eaf9611392e16b56cf05352eda37b1a
liuhao <58254451@qq.com>
2020-09-30 15:19:57 +0800

    first problem is ok

--- cs480/pa1/map.py
+++ cs480/pa1/map.py
@@ -3,9 +3,12 @@ from search import breadth_first_tree_search
 from search import breadth_first_graph_search
 from search import depth_first_graph_search
 from search import depth_first_tree_search
+from search import best_first_graph_search
 from search import uniform_cost_search
 from search import astar_search
 import sys
+
+
 # [DFTS, DFGS, BFTS, BFGS, UCTS, UCGS, GBFTS, GBFGS, ASTS, ASGS]
 state_list = {}
 h={}
@@ -37,6 +40,33 @@ def file_read(filename):
 			tmp = r.replace('\n', '').split(' ')
 			h[tmp[0]]=int(tmp[1])
 
+def return_node_from_search(search_class,problem_) -> object:
+
+	search_class=search_class.upper()
+	res_node=None
+	if search_class=='DFTS':
+		res_node=depth_first_tree_search(problem_)
+	elif search_class=='DFGS':
+		res_node = depth_first_graph_search(problem_)
+	elif search_class=='BFTS':
+		res_node = breadth_first_tree_search(problem_)
+	elif search_class=='BFGS':
+		res_node = breadth_first_graph_search(problem_)
+	elif search_class=='UCTS':
+		res_node = uniform_cost_search(problem_)
+	elif search_class=='UCGS':
+		res_node = uniform_cost_search(problem_)
+	elif search_class=='ASTS':
+		res_node = astar_search(problem_)
+	elif search_class=='ASGS':
+		res_node = astar_search(problem_)
+	elif search_class=='GBFTS':
+		res_node = best_first_graph_search(problem_,lambda n:h[n.state])
+	elif search_class=='GBFGS':
+		res_node = best_first_graph_search(problem_,lambda n:h[n.state])
+	else:
+		print ('Parameter 2 is incorrect. Please enter one of the following parameters [DFTS, DFGS, BFTS, BFGS, UCTS, UCGS, GBFTS, GBFGS, ASTS, ASGS] ')
+	return res_node
 class travel(Problem):
 	def __init__(self, initial, goal):
 		super().__init__(initial,goal)
@@ -76,24 +106,21 @@ class travel(Problem):
 
 if __name__ == '__main__':
 	input_file = sys.argv[1]
-	# search_algo_str = sys.argv[2]
+	search_algo_str = sys.argv[2]
 	# TODO implement
 	file_read(input_file)
-	# goal_node = # TODO call the appropriate search function with appropriate parameters
+
+	goal_node =return_node_from_search(str(search_algo_str),travel(initial, goal)) # TODO call the appropriate search function with appropriate parameters
 
 	# Do not change the code below.
 
-	# if goal_node is not None:
-	# 	print("Solution path", goal_node.solution())
-	# 	print("Solution cost", goal_node.path_cost)
-	# else:S
-	# 	print("No solution was found.")
+	if goal_node is not None:
+		print("Solution path", goal_node.solution())
+		print("Solution cost", goal_node.path_cost)
+	else:
+		print("No solution was found.")
 
-	p = travel(initial, goal)
-	res=astar_search(p)
-	#res=depth_first_tree_search(p)
-	print(res.solution())
-	print(res.path_cost)
+
 
 
 
