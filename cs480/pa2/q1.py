31704b1bb577eff19a15ad40f40648f33b894585
liuhao <58254451@qq.com>
2020-10-16 18:54:25 +0800

    Update q1.py

--- cs480/pa2/q1.py
+++ cs480/pa2/q1.py
@@ -16,26 +16,29 @@ def file_read(filename):
             for j in range(0, len(r)):
                 board[i+1, j+1] = r[j].upper()
             i += 1
-
-    return board
+    initial=GameState(to_move='', utility=0, board=board, moves='')
+    return initial
 
 
 class game_problem():
 
-    def __init__(self, _players, init_board={},h=3, v=3, k=3):
-        self.h = h
-        self.v = v
-        self.k = k
+    def __init__(self, _players, initial_state):
+        self.h=0
+        self.v=0
+        self.k=0
+        for k in initial_state.board.keys():
+            self.h=k[0] if k[0]>self.h else self.h
+            self.v = k[1] if k[1] > self.v else self.v
+        self.k = max(self.h,self.v)
         self.players = [r for r in _players]
-        moves = [(x, y) for x in range(1, h + 1) for y in range(1, v + 1)]
+        moves = [(x, y) for x in range(1, self.h + 1) for y in range(1, self.v + 1)]
 
-        for k in init_board.keys():
+        for k in initial_state.board.keys():
 
-            if init_board[k].upper()=='X' or init_board[k].upper()=='O':
+            if initial_state.board[k].upper()=='X' or initial_state.board[k].upper()=='O':
                 moves.remove(k)
 
-        self.initial = GameState(to_move='', utility=0, board=init_board, moves=moves)
-        self.initial = GameState(to_move=self.to_move(self.initial), utility=0, board=init_board, moves=moves)
+        self.initial = GameState(to_move=self.to_move(initial_state), utility=0, board=initial_state.board, moves=moves)
 
 
 
@@ -163,12 +166,13 @@ def minmax_decision(state, game):
 
 if __name__ == '__main__':
     input_file = sys.argv[1]
-
     # TODO implement
-    game = game_problem(['X', 'O'],file_read(input_file))
-    # minmax_decision(,game)
+    initial=file_read(input_file)
+    game = game_problem(['X', 'O'],initial)
+    #print(minmax_decision(initial,game))
 
     print('Whose turn is it in this state?')
+    print(game.to_move(initial))
     # TODO: print either X or O
     print(
         'If both X and O play optimally from this state, does X have a guaranteed win, guaranteed loss, or guaranteed draw')
